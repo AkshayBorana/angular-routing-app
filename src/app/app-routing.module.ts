ccb0c055a2e7599ef54cbd9256d0c64e9c5f9621
Akshay Borana <akshayborana1@gmail.com>
2020-11-09 16:07:27 -0500

    feat: defining basic routes and adding classes based on active route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Setting up components for routing...
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+const routes: Routes = [
+  {
+    path: 'first-component',
+    component: FirstComponent
+  },
+  {
+    path: 'second-component',
+    component: SecondComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
