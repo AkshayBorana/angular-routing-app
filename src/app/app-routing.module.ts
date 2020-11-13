c4b925f90405c8d4c5982e3b09bbf1474416b17d
Akshay Borana <akshayborana1@gmail.com>
2020-11-13 13:30:35 -0500

    refactor: re-initializing the router app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Setting up components for routing...
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
 
 const routes: Routes = [
-  {
-    path: 'first-component',
-    component: FirstComponent
-  },
-  {
-    path: 'second-component',
-    component: SecondComponent
-  }
+
 ];
 
 @NgModule({
