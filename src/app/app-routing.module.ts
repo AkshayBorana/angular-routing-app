3e13d8f26b08c5e94d4667a5da16923786f04fc1
Akshay Borana <akshayborana1@gmail.com>
2020-11-16 15:18:56 -0500

    feat: re-structuring app, and adding new components, services to heroes module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Setting up components for routing...
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
+// import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
@@ -11,16 +11,16 @@ const appRoutes: Routes = [
     path: 'crisis-center',
     component: CrisisListComponent
   },
-  {
-    path: 'heroes',
-    component: HeroListComponent
-  },
-  {
-    path: '',
-    // This is an absolute redirect, as the redirect to property starts with '/' in it's path '/heroes'...
-    redirectTo: '/heroes',
-    pathMatch: 'full'
-  },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent
+  // },
+  // {
+  //   path: '',
+  //   // This is an absolute redirect, as the redirect to property starts with '/' in it's path '/heroes'...
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
   {
     path: '**',
     component: PageNotFoundComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,22 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
+// import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeroesModule } from './heroes/heroes.module';
+
+const Components = [
+  AppComponent,
+  CrisisListComponent,
+  // HeroListComponent,
+  PageNotFoundComponent
+];
+const Modules = [HeroesModule];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CrisisListComponent,
-    HeroListComponent,
-    PageNotFoundComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
+  declarations: [...Components],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, ...Modules],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
